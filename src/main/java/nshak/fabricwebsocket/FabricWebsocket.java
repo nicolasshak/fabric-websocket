4cdddc8180354f02f7e5e2c813b99b538fc86924
Nicolas Shak <nicolas.shak@gmail.com>
2021-03-09 16:46:55 -0800

    Initial commit

---
+++ src/main/java/nshak/fabricwebsocket/FabricWebsocket.java
@@ -0,0 +1,10 @@
+package nshak.fabricwebsocket;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricWebsocket implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/nshak/fabricwebsocket/client/FabricWebsocketClient.java
@@ -0,0 +1,13 @@
+package nshak.fabricwebsocket.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FabricWebsocketClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
